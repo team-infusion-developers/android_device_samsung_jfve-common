1e2f7abd3125888152a07342690856c426d2235c
Exodusnick <Christiancanale@gmx.de>
2019-02-19 08:11:18 +0100

    Update jfve-common.mk

--- jfve-common.mk
+++ jfve-common.mk
@@ -319,4 +319,4 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product, device/samsung/qcom-common/qcom-common.mk)
 
 # Get non-open-source specific aspects
-$(call inherit-product, vendor/samsung/jfvelte/jfvelte-vendor.mk)
+$(call inherit-product, vendor/samsung/jf-common/jf-common-vendor.mk)
