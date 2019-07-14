9914285608f6c199decac3e7b69974f681e41814
Shakthivel Nadar <45127011+Sonic-sakthivel123@users.noreply.github.com>
2019-07-14 14:04:40 +0530

    Update Media.mk

--- props/Media.mk
+++ props/Media.mk
@@ -14,3 +14,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
     camera2.portability.force_api=1 \
     media.stagefright.legacyencoder=true \
 	media.stagefright.less-secure=true
+
+#Media quality
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.media.enc.jpeg.quality=100 \
+    ro.media.dec.jpeg.memcap=8000000 \
+    ro.media.enc.hprof.vid.bps=8000000 \
+    ro.media.dec.aud.wma.enabled=1 \
+    ro.media.dec.vid.wmv.enabled=1 \
+    ro.media.cam.preview.fps=0 \
+    ro.media.codec_priority_for_thumb=so \
